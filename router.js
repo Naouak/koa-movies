270b7d15ae45e489266bce56f6862c5f0cd76b94
Naouak <tardiel@gmail.com>
2017-01-09 22:16:13 +0100

    Making files eslint(almost-air-bnb) compliant.

--- router.js
+++ router.js
@@ -1,57 +1,57 @@
-let fs = require('fs');
-let path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-class Router{
-  constructor(options = {}){
-    this._controllerDirectory = options.directory || path.join(__dirname, "controllers");
+class Router {
+  constructor(options = {}) {
+    this._controllerDirectory = options.directory || path.join(__dirname, 'controllers');
   }
-  
+
+  get controllerDirectory() {
+    return this._controllerDirectory;
+  }
+
   /**
    *
    * @returns {Promise}
    */
-  getControllers(){
+  getControllers() {
     return new Promise(
-      (resolve, reject)=>
-        fs.readdir(
-          this._controllerDirectory,
-          (err, files) => err?reject(err):resolve(files)
-        )
-    );
+      (resolve, reject) =>
+        (fs.readdir(this._controllerDirectory, (err, files) => (err ? reject(err) : resolve(files)))));
   }
-  
-  *route(method, url, ctx, next){
-   let [, controller = "home", action = "index", ...vars] = url.split('/');
-   let controllers = yield this.getControllers();
-   
-   
-   if(controllers.indexOf(controller+".js") < 0){
-     // No controller found : 404
-     console.error("Controller "+controller+" not found.");
-     ctx.response.status = 404;
-     return;
-   }
-   
-   let controllerObject = require(path.join(this._controllerDirectory, controller+".js"));
-   
-   action=action||"index";
-   
-   if(!controllerObject || !controllerObject[action]){
-     console.error("Action "+controller+"."+action+" not found.");
-     ctx.response.status = 404;
-     return;
-   }
-   
-   let generator = controllerObject[action].bind(ctx, next, ...vars);
-   yield* generator();
-   
-  }
-  
-  middleware(){
-    let self = this;
-    return function *(next){
-      yield *self.route(this.request.method, this.request.url, this, next);
+
+  * route(method, url, ctx, next) {
+    const [, controller = 'home', action = 'index', ...vars] = url.split('/');
+    const controllers = yield this.getControllers();
+
+
+    if (controllers.indexOf(`${controller}.js`) < 0) {
+      // No controller found : 404
+      console.error(`Controller ${controller} not found.`);
+      ctx.response.status = 404;
+      return;
     }
+
+    /* eslint import/no-dynamic-require: 0 global-require: 0 */
+    const controllerObject = require(path.join(this._controllerDirectory, `${controller}.js`));
+
+    const finalAction = action || 'index';
+
+    if (!controllerObject || !controllerObject[finalAction]) {
+      console.error(`Action ${controller}.${finalAction} not found.`);
+      ctx.response.status = 404;
+      return;
+    }
+
+    const generator = controllerObject[finalAction].bind(ctx, next, ...vars);
+    yield* generator();
+  }
+
+  middleware() {
+    const self = this;
+    return function* middleware(next) {
+      yield* self.route(this.request.method, this.request.url, this, next);
+    };
   }
 }
 
--- test/router.js
+++ test/router.js
@@ -1,95 +1,97 @@
-let assert = require('assert');
-let should = require('should');
-let path = require('path');
+/* eslint-env node, mocha */
+// noinspection Eslint
+const should = require('should');
+const path = require('path');
 
-let Router = require('../router');
-describe('Router', function(){
-  it("should provide a default path to controllers directory", function(){
-    let router = new Router();
-    router._controllerDirectory.should.equal(path.join(__dirname, "../controllers"));
+const Router = require('../router');
+
+describe('Router', () => {
+  it('should provide a default path to controllers directory', () => {
+    const router = new Router();
+    router.controllerDirectory.should.equal(path.join(__dirname, '../controllers'));
   });
-  
-  it("should find all controllers in the directory", function(done){
-    let router = new Router({directory: path.join(__dirname,'fixtures/controllers')});
-    
+
+  it('should find all controllers in the directory', (done) => {
+    const router = new Router({ directory: path.join(__dirname, 'fixtures/controllers') });
+
     router.getControllers()
-      .then(files=>{
+      .then((files) => {
         files.should.have.lengthOf(1);
-        files.should.have.property(0,'test.js');
+        files.should.have.property(0, 'test.js');
         done();
       })
       .catch(done);
   });
-  
-  it("should fail on unknown path", function(done){
-    let router = new Router({directory: path.join(__dirname,"tagada")});
-    
+
+  it('should fail on unknown path', (done) => {
+    const router = new Router({ directory: path.join(__dirname, 'tagada') });
+
     router.getControllers()
-      .then(files=>{
-        done("it should not have return files");
+      .then(() => {
+        done('it should not have return files');
       })
       .catch(() => done());
   });
-  
-  it("should return a generator for the middleware", function(){
-    let router = new Router({directory: path.join(__dirname,'fixtures/controllers')});
-    let middleware = router.middleware().bind({request: { url: '/', method: 'GET'}});
-    let returnValue = middleware().next();
-    
+
+  it('should return a generator for the middleware', () => {
+    const router = new Router({ directory: path.join(__dirname, 'fixtures/controllers') });
+    const middleware = router.middleware().bind({ request: { url: '/', method: 'GET' } });
+    const returnValue = middleware().next();
+
     returnValue.should.have.property('value');
     returnValue.should.have.property('done');
   });
-  
-  it("should yield the correct action for given url", function(done){
-    let router = new Router({directory: path.join(__dirname,'fixtures/controllers')});
-    
-    let route = router.route(null,"/test/test");
-    
+
+  it('should yield the correct action for given url', (done) => {
+    const router = new Router({ directory: path.join(__dirname, 'fixtures/controllers') });
+
+    const route = router.route(null, '/test/test');
+
     route.next().value
-      .then(value=>route.next(value))
-      .then(function(actionYield){
-        actionYield.value.should.equal("tagada");
+      .then(value => (route.next(value)))
+      .then((actionYield) => {
+        actionYield.value.should.equal('tagada');
         done();
       })
       .catch(done);
   });
-  
-  it("should redirect to action index if no action is provided", function(done){
-    let router = new Router({directory: path.join(__dirname,'fixtures/controllers')});
-    let ctx = {};
-    let route = router.route(null,"/test/",ctx);
-    
+
+  it('should redirect to action index if no action is provided', (done) => {
+    const router = new Router({ directory: path.join(__dirname, 'fixtures/controllers') });
+    const ctx = {};
+    const route = router.route(null, '/test/', ctx);
+
     route.next().value
-      .then(value=>route.next(value))
-      .then(function(){
-        ctx.body.should.equal("index");
+      .then(value => (route.next(value)))
+      .then(() => {
+        ctx.body.should.equal('index');
         done();
       })
       .catch(done);
   });
-  
-  it("should 404 on unknown controller", function(done){
-    let router = new Router({directory: path.join(__dirname,'fixtures/controllers')});
-    let ctx = {response:{}};
-    let route = router.route(null,"/tagada/test",ctx);
-    
+
+  it('should 404 on unknown controller', (done) => {
+    const router = new Router({ directory: path.join(__dirname, 'fixtures/controllers') });
+    const ctx = { response: {} };
+    const route = router.route(null, '/tagada/test', ctx);
+
     route.next().value
-      .then(value=>route.next(value))
-      .then(function(){
+      .then(value => (route.next(value)))
+      .then(() => {
         ctx.response.status.should.equal(404);
         done();
       })
       .catch(done);
   });
-  
-  it("should 404 on unknown action", function(done){
-    let router = new Router({directory: path.join(__dirname,'fixtures/controllers')});
-    let ctx = {response:{}};
-    let route = router.route(null,"/test/tsouintsouin",ctx);
-    
+
+  it('should 404 on unknown action', (done) => {
+    const router = new Router({ directory: path.join(__dirname, 'fixtures/controllers') });
+    const ctx = { response: {} };
+    const route = router.route(null, '/test/tsouintsouin', ctx);
+
     route.next().value
-      .then(value=>route.next(value))
-      .then(function(){
+      .then((value) => { route.next(value); })
+      .then(() => {
         ctx.response.status.should.equal(404);
         done();
       })
